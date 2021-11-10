9107c52fb8a8853c42ba9d0a0b2dd35797649f2e
Erick Espinosa <erick.lodion@gmail.com>
2021-11-10 11:48:21 -0300

    Initial commit

---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl = 'http://localhost:3001/products'
+
+  constructor(
+    private snackBar: MatSnackBar, 
+    private http: HttpClient) 
+    { }
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'got it', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+    })
+  }
+
+  create(product: Product) : Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  read() : Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  readById(id: string) : Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  update(product: Product) : Observable<Product>{
+    return this.http.put<Product>(`${this.baseUrl}/${product.id}`, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  delete(id: string) : Observable<Product>{
+    return this.http.delete<Product>(`${this.baseUrl}/${id}`).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e: any): Observable<any>{
+    this.showMessage('Ocorreu um erro!', true)
+    return EMPTY
+  }
+
+}
